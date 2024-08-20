0a9844e6b4971d4e19524b38023843bc4996f94f
fwiffo <fwiffo@>
2024-08-21 00:43:31 +0300

    minor refactor

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -9,34 +9,16 @@ import (
 	"github.com/GaryShem/ya-metrics.git/internal/shared/logging"
 )
 
-var buildVersion string = "0.20"
-var buildDate string = "2024-08-07"
-var buildCommit string
-
-func logVersion() {
-	output := "N/A"
-	if buildVersion != "" {
-		output = buildVersion
-	}
-	logging.Log.Infof("Build version: %s", output)
-	output = "N/A"
-	if buildDate != "N/A" {
-		output = buildDate
-	}
-	logging.Log.Infof("Build date: %s", output)
-	output = "N/A"
-	if buildCommit != "" {
-		output = buildCommit
-	}
-	logging.Log.Infof("Build commit: %s", output)
-}
+const buildVersion string = "0.20"
+const buildDate string = "2024-08-07"
+const buildCommit string = ""
 
 func main() {
 	err := logging.InitializeZapLogger("Info")
 	if err != nil {
 		log.Fatal(err)
 	}
-	logVersion()
+	logging.LogVersion(buildVersion, buildDate, buildCommit)
 	agentFlags := new(config.AgentFlags)
 	config.ParseFlags(agentFlags)
 	logging.Log.Infoln("client starting with flags",
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,34 +8,16 @@ import (
 	"github.com/GaryShem/ya-metrics.git/internal/shared/logging"
 )
 
-var buildVersion string = "0.20"
-var buildDate string = "2024-08-07"
-var buildCommit string
-
-func logVersion() {
-	output := "N/A"
-	if buildVersion != "" {
-		output = buildVersion
-	}
-	logging.Log.Infof("Build version: %s", output)
-	output = "N/A"
-	if buildDate != "N/A" {
-		output = buildDate
-	}
-	logging.Log.Infof("Build date: %s", output)
-	output = "N/A"
-	if buildCommit != "" {
-		output = buildCommit
-	}
-	logging.Log.Infof("Build commit: %s", output)
-}
+const buildVersion string = "0.20"
+const buildDate string = "2024-08-07"
+const buildCommit string = "n/a"
 
 func main() {
 	err := logging.InitializeZapLogger("Info")
 	if err != nil {
 		log.Fatal(err)
 	}
-	logVersion()
+	logging.LogVersion(buildVersion, buildDate, buildCommit)
 	serverFlags := new(config.ServerFlags)
 	config.ParseFlags(serverFlags)
 	err = app.RunServer(serverFlags)
